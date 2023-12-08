e2891b62c1e02111609c30d56179f99436b9bd53
Andrei Paleshka <118306084+andrplko@users.noreply.github.com>
2023-12-08 08:54:55 +0100

    Implement sign in and sign up forms (#2)
    * feat: implement sign in and sign up forms
    
    * fix: change route names
    
    * fix: fix logo name
    
    * fix: change form title

---
+++ src/constants/routes.ts
@@ -0,0 +1,6 @@
+export const enum Routes {
+  WELCOME = '/',
+  MAIN = '/main',
+  SIGN_IN = '/signin',
+  SIGN_UP = '/signup',
+}
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,9 +1,10 @@
+import SignUpForm from '@/components/SignUpForm';
 import styles from '@/styles/SignUpPage.module.scss';
 
 const SignUpPage = () => {
   return (
     <div className={styles.container}>
-      <h1>Sign Up Page</h1>
+      <SignUpForm />
     </div>
   );
 };
