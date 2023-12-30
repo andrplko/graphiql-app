4b84de81e974bb0ead441f5db109058083c07e1d
Andrei Paleshka <andrepoleshko@gmail.com>
2023-12-30 15:34:42 +0100

    feat: add  local provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,17 @@ import type { AppProps } from 'next/app';
 import Layout from '@/components/Layout';
 import useLoader from '@/hooks/useLoader';
 import '@/styles/globals.scss';
+import LocaleProvider from '@/context/locales';
 
 const App = ({ Component, pageProps }: AppProps) => {
   useLoader();
 
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <LocaleProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </LocaleProvider>
   );
 };
 
