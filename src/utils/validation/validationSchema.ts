d7f0badda009f9d70f0d3f5a7372d91e6b284bb3
Andrei Paleshka <andrepoleshko@gmail.com>
2023-12-30 15:36:23 +0100

    feat: add locale data to validation schema

--- src/utils/validation/validationSchema.ts
+++ src/utils/validation/validationSchema.ts
@@ -1,23 +1,28 @@
 import { z } from 'zod';
+import { LocaleData } from '@/context/locales';
 
-const validationSchema = z.object({
-  email: z
-    .string({ required_error: 'Email is required' })
-    .min(1, 'Email is required')
-    .email('Please enter a valid email address'),
-  password: z
-    .string({ required_error: 'Password is required' })
-    .min(1, 'Password is required')
-    .min(8, 'Password must contain minimum 8 symbols')
-    .refine((value) => /^(?=.*\p{L})/gu.test(value), {
-      message: 'Password must contain at least one letter',
-    })
-    .refine((value) => /^(?=.*[0-9])/.test(value), {
-      message: 'Password must contain one digit',
-    })
-    .refine((value) => /^(?=.*[!@#\$%\^&\*])/.test(value), {
-      message: 'Password must contain one special character',
-    }),
-});
+const validationSchema = (localeData: LocaleData) => {
+  const { validation } = localeData;
+
+  return z.object({
+    email: z
+      .string({ required_error: validation.email.required })
+      .min(1, validation.email.required)
+      .email(validation.email.message),
+    password: z
+      .string({ required_error: validation.password.required })
+      .min(1, validation.password.required)
+      .min(8, validation.password.messages.eight_symbols)
+      .refine((value) => /^(?=.*\p{L})/gu.test(value), {
+        message: validation.password.messages.one_letter,
+      })
+      .refine((value) => /^(?=.*[0-9])/.test(value), {
+        message: validation.password.messages.one_digit,
+      })
+      .refine((value) => /^(?=.*[!@#\$%\^&\*])/.test(value), {
+        message: validation.password.messages.one_special,
+      }),
+  });
+};
 
 export default validationSchema;
